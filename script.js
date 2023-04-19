90550b937214fa0abd21c7c956dca4e08d20f002
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-04-18 20:52:44 -0400

    added variables i will be using

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  if (password) {passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// created variables
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
