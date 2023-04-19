98fccc2a78e99dbca2f652436b9e37b5b4fffbd4
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-04-18 21:47:40 -0400

    added functions for upper case letters and start of generating ps

--- script.js
+++ script.js
@@ -19,9 +19,29 @@ var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
+var choices;
 
 // characters that can be included
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 space = [];
+
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+alpha2 = alpha.map(toUpper);
+
+// function to generate password
+function generatePassword() {
+  enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+} if (!enter) {
+      alert("This needs a value");
+  } else if (enter < 8 || enter > 128) {
+      enter = parseInt(prompt("You must choose between 8 and 128"));
+} else {
+      confirmNumber = confirm("Will this contain numbers?");
+      confirmCharacter = confirm("Will this contain special characters?");
+      confirmUppercase = confirm("Will this contain Uppercase letters?");
+      confirmLowercase = confirm("Will this contain Lowercase letters?");
+  };
